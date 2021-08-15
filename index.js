6d95c45a1a2518cf2920f45437e67f7bff5195ad
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2021-08-15 11:05:24 +0000

    Something

--- index.js
+++ index.js
@@ -6,21 +6,6 @@ let userName = readlineSync.question('Enter your name please? ')
 console.log(`Welcome! Mr/Mrs.${userName}
 \n`);
 
-function askQuestion(myQuestions, myAnswers){
-    // console.log(myQuestions, myAnswers)
-
-    var askMyAnswer = readlineSync.question(myQuestions);
-
-    if(askMyAnswer.toUpperCase() === myAnswers.toUpperCase()){
-      score = score + 1
-      console.log(`\n Wow! You are right...your score is ${score} \n`)
-    }else{
-      score = score-1 
-      console.log(`\n ohh! you are worng...you socre is ${score} \n`)
-    }
-
-
-}
 
  var quizs = [{question: chalk.bgBlue('The real name of Pritam Kumar is?  \n\n') ,answer: 'Shubham Kumar'
  },
@@ -38,6 +23,22 @@ function askQuestion(myQuestions, myAnswers){
    answer: 'Tom and Jerry'
  }];
 
+function askQuestion(myQuestions, myAnswers){
+    // console.log(myQuestions, myAnswers)
+
+    var askMyAnswer = readlineSync.question(myQuestions);
+
+    if(askMyAnswer.toUpperCase() === myAnswers.toUpperCase()){
+      score = score + 1
+      console.log(`\n Wow! You are right...your score is ${score} \n`)
+    }else{
+      score = score-1 
+      console.log(`\n ohh! you are worng...you socre is ${score} \n`)
+    }
+
+
+}
+
  
 for(i=0; i<quizs.length; i++){
   var currentQuestion = quizs[i]
@@ -46,4 +47,4 @@ for(i=0; i<quizs.length; i++){
 }
 
 
- console.log(`\n Than you for playing Mr/Mrs. ${userName} your total score is ${score} \n`)
+ console.log(`\n Thank you for playing Mr/Mrs. ${userName} your total score is ${score} \n`)
